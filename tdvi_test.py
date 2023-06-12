dd188524fc91c472910d2ab0254bf08683f8e5e4
Yang Xianglin <yxlyzxl@gmail.com>
2023-06-12 19:45:18 +0800

    minor

---
+++ tdvi_test.py
@@ -0,0 +1,102 @@
+########################################################################################################################
+#                                                          IMPORT                                                      #
+########################################################################################################################
+import torch
+import sys
+import os
+import json
+import time
+import numpy as np
+import argparse
+
+from torch.utils.data import DataLoader
+from torch.utils.data import WeightedRandomSampler
+from umap.umap_ import find_ab_params
+
+from singleVis.custom_weighted_random_sampler import CustomWeightedRandomSampler
+from singleVis.SingleVisualizationModel import VisModel
+from singleVis.losses import UmapLoss, ReconstructionLoss, SingleVisLoss, LocalTemporalLoss, SmoothnessLoss
+from singleVis.edge_dataset import DVIDataHandler, LocalTemporalDataHandler
+from singleVis.trainer import DVITrainer, SingleVisTrainer, LocalTemporalTrainer
+from singleVis.data import NormalDataProvider
+from singleVis.spatial_edge_constructor import LocalSpatialTemporalEdgeConstructor, SingleEpochSpatialEdgeConstructor
+from singleVis.projector import DVIProjector
+from singleVis.eval.evaluator import Evaluator
+from singleVis.visualizer import visualizer
+########################################################################################################################
+#                                                     DVI PARAMETERS                                                   #
+########################################################################################################################
+"""DVI with semantic temporal edges"""
+VIS_METHOD = "tDVI" # DeepVisualInsight
+
+########################################################################################################################
+#                                                     LOAD PARAMETERS                                                  #
+########################################################################################################################
+parser = argparse.ArgumentParser(description='Process hyperparameters...')
+parser.add_argument('--content_path', type=str)
+args = parser.parse_args()
+
+CONTENT_PATH = args.content_path
+sys.path.append(CONTENT_PATH)
+with open(os.path.join(CONTENT_PATH, "config.json"), "r") as f:
+    config = json.load(f)
+config = config[VIS_METHOD]
+
+SETTING = config["SETTING"]
+CLASSES = config["CLASSES"]
+DATASET = config["DATASET"]
+PREPROCESS = config["VISUALIZATION"]["PREPROCESS"]
+GPU_ID = config["GPU"]
+EPOCH_START = config["EPOCH_START"]
+EPOCH_END = config["EPOCH_END"]
+EPOCH_PERIOD = config["EPOCH_PERIOD"]
+EPOCH_NAME = config["EPOCH_NAME"]
+
+# Training parameter (subject model)
+TRAINING_PARAMETER = config["TRAINING"]
+NET = TRAINING_PARAMETER["NET"]
+LEN = TRAINING_PARAMETER["train_num"]
+
+# Training parameter (visualization model)
+VISUALIZATION_PARAMETER = config["VISUALIZATION"]
+LAMBDA1 = VISUALIZATION_PARAMETER["LAMBDA1"]
+B_N_EPOCHS = VISUALIZATION_PARAMETER["BOUNDARY"]["B_N_EPOCHS"]
+L_BOUND = VISUALIZATION_PARAMETER["BOUNDARY"]["L_BOUND"]
+ENCODER_DIMS = VISUALIZATION_PARAMETER["ENCODER_DIMS"]
+DECODER_DIMS = VISUALIZATION_PARAMETER["DECODER_DIMS"]
+S_N_EPOCHS = VISUALIZATION_PARAMETER["S_N_EPOCHS"]
+T_N_EPOCHS = VISUALIZATION_PARAMETER["T_N_EPOCHS"]
+N_NEIGHBORS = VISUALIZATION_PARAMETER["N_NEIGHBORS"]
+PATIENT = VISUALIZATION_PARAMETER["PATIENT"]
+MAX_EPOCH = VISUALIZATION_PARAMETER["MAX_EPOCH"]
+
+VIS_MODEL_NAME = VISUALIZATION_PARAMETER["VIS_MODEL_NAME"]
+EVALUATION_NAME = VISUALIZATION_PARAMETER["EVALUATION_NAME"]
+
+# Define hyperparameters
+DEVICE = torch.device("cuda:{}".format(GPU_ID) if torch.cuda.is_available() else "cpu")
+
+import Model.model as subject_model
+net = eval("subject_model.{}()".format(NET))
+
+########################################################################################################################
+#                                                    TRAINING SETTING                                                  #
+########################################################################################################################
+# Define data_provider
+data_provider = NormalDataProvider(CONTENT_PATH, net, EPOCH_START, EPOCH_END, EPOCH_PERIOD, device=DEVICE, classes=CLASSES, epoch_name=EPOCH_NAME, verbose=1)
+# Define visualization models
+model = VisModel(ENCODER_DIMS, DECODER_DIMS)
+
+# Define Projector
+projector = DVIProjector(vis_model=model, content_path=CONTENT_PATH, vis_model_name=VIS_MODEL_NAME, epoch_name=EPOCH_NAME, device=DEVICE)
+
+vis = visualizer(data_provider, projector, 200, "tab10")
+save_dir = os.path.join(data_provider.content_path, "img")
+if not os.path.exists(save_dir):
+    os.mkdir(save_dir)
+evaluator = Evaluator(data_provider, projector, metric="euclidean")
+
+# save_dir = os.path.join(data_provider.content_path, "img")
+# vis.savefig(EPOCH_START, path=os.path.join(save_dir, "{}_{}_{}.png".format(DATASET, EPOCH_START, VIS_METHOD)))
+evaluator.save_epoch_eval(EPOCH_START, 15, temporal_k=5, file_name="{}".format(EVALUATION_NAME))
+

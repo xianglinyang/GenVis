531b64f590d240920accfc9ee79bc22493560412
Yang Xianglin <yxlyzxl@gmail.com>
2023-09-01 22:25:31 +0800

    try light continual norm

--- singleVis/vis_models/continual_norm.py
+++ singleVis/vis_models/continual_norm.py
@@ -80,28 +80,45 @@ class ContinualNormAE(BaseVisModel):
         # Build Encoder
         modules = list()
         for i in range(0, len(self.encoder_dims)-2):
-            modules.append(
-                nn.Sequential(
-                nn.Linear(self.encoder_dims[i], self.encoder_dims[i+1]),
-                # ContinualNorm(8, self.encoder_dims[i+1]),
-                CN(self.encoder_dims[i+1]),
-                nn.ReLU(True) 
+            if i==0:
+                modules.append(
+                    nn.Sequential(
+                    nn.Linear(self.encoder_dims[i], self.encoder_dims[i+1]),
+                    CN(self.encoder_dims[i+1]),
+                    nn.ReLU(True) 
+                    )
                 )
-            )
+            else:
+                modules.append(
+                    nn.Sequential(
+                    nn.Linear(self.encoder_dims[i], self.encoder_dims[i+1]),
+                    nn.BatchNorm1d(self.encoder_dims[i+1]),
+                    # CN(self.encoder_dims[i+1]),
+                    nn.ReLU(True) 
+                    )
+                )
         modules.append(nn.Linear(self.encoder_dims[-2], self.encoder_dims[-1]))
         self.encoder = nn.Sequential(*modules)
 
         # Build Decoder
         modules = list()
         for i in range(0, len(self.decoder_dims)-2):
-            modules.append(
-                nn.Sequential(
-                    nn.Linear(self.decoder_dims[i], self.decoder_dims[i+1]),
-                    # ContinualNorm(8, self.decoder_dims[i+1]),
-                    CN(self.decoder_dims[i+1]),
-                    nn.ReLU(True)
+            if i == 0:
+                modules.append(
+                    nn.Sequential(
+                        nn.Linear(self.decoder_dims[i], self.decoder_dims[i+1]),
+                        CN(self.decoder_dims[i+1]),
+                        nn.ReLU(True)
+                    )
                 )
-            )
+            else:
+                modules.append(
+                    nn.Sequential(
+                        nn.Linear(self.decoder_dims[i], self.decoder_dims[i+1]),
+                        nn.BatchNorm1d(self.decoder_dims[i+1]),
+                        nn.ReLU(True)
+                    )
+                )
         modules.append(nn.Linear(self.decoder_dims[-2], self.decoder_dims[-1]))
         self.decoder = nn.Sequential(*modules)
     

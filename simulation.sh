bd51256e2888c5de399213c7f577163f03e565ae
Yang Xianglin <yxlyzxl@gmail.com>
2024-05-10 05:54:13 +0000

    minor

--- simulation.sh
+++ simulation.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # content_path="/home/yangxl21/DVI_data/ViT_CIFAR10"
 # END=15
 # # N_jobs=10
@@ -14,7 +13,63 @@
 # # wait
 # echo "All Done!"
 
-content_path="/home/yangxl21/DVI_data/cl"
+# content_path="/home/yangxl21/DVI_data/cl"
+# resumes=(0 1 2 3 4)
+# iterations=(1 2 3 4 5)
+
+# for i in {0..4}; 
+# do
+#     nohup python tdvi.py --content_path ${content_path} -i ${iterations[$i]} -r ${resumes[$i]} 1>> ${content_path}/tdvi_log 2>&1
+# done
+
+# content_path="/home/yangxl21/DVI_data/class_ewc"
+# resumes=(0 1 2 3 4)
+# iterations=(1 2 3 4 5)
+
+# for i in {0..4}; 
+# do
+#     nohup python tdvi.py --content_path ${content_path} -i ${iterations[$i]} -r ${resumes[$i]} 1>> ${content_path}/tdvi_log 2>&1
+# done
+
+
+content_path="/home/yangxl21/DVI_data/class_fromp"
+resumes=(0 1 2 3 4)
+iterations=(1 2 3 4 5)
+
+for i in {0..4}; 
+do
+    nohup python tdvi.py --content_path ${content_path} -i ${iterations[$i]} -r ${resumes[$i]} 1>> ${content_path}/tdvi_log 2>&1
+done
+
+
+content_path="/home/yangxl21/DVI_data/class_replay"
+resumes=(0 1 2 3 4)
+iterations=(1 2 3 4 5)
+
+for i in {0..4}; 
+do
+    nohup python tdvi.py --content_path ${content_path} -i ${iterations[$i]} -r ${resumes[$i]} 1>> ${content_path}/tdvi_log 2>&1
+done
+
+
+# content_path="/home/yangxl21/DVI_data/domain_ewc"
+# resumes=(0 1 2 3 4)
+# iterations=(1 2 3 4 5)
+
+# for i in {0..4}; 
+# do
+#     nohup python tdvi.py --content_path ${content_path} -i ${iterations[$i]} -r ${resumes[$i]} 1>> ${content_path}/tdvi_log 2>&1
+# done
+
+content_path="/home/yangxl21/DVI_data/domain_replay"
+resumes=(0 1 2 3 4)
+iterations=(1 2 3 4 5)
+
+for i in {0..4}; 
+do
+    nohup python tdvi.py --content_path ${content_path} -i ${iterations[$i]} -r ${resumes[$i]} 1>> ${content_path}/tdvi_log 2>&1
+done
+content_path="/home/yangxl21/DVI_data/domain_fromp"
 resumes=(0 1 2 3 4)
 iterations=(1 2 3 4 5)
 
---
+++ tdvi_run.sh
@@ -0,0 +1,72 @@
+#! /bin/bash
+# ###############################################################
+content_path="/home/yangxl21/DVI_data/ResNet_CIFAR10"
+
+resumes=()
+# cold start
+resumes1=($(seq 0 1 9))
+# parallel run
+resumes2=(9)
+resumes3=($(seq 9 1 197))
+resumes+=( "${resumes1[@]}" "${resumes2[@]}" "${resumes3[@]}")
+iterations=($(seq 1 1 200))
+for i in {0..199}; 
+do
+    python tdvi.py --content_path ${content_path} -i ${iterations[$i]} -r ${resumes[$i]} 1> ${content_path}/tdvi_log 2>&1
+done
+# ###############################################################
+content_path="/home/yangxl21/DVI_data/ResNet_CIFAR100"
+
+resumes=()
+# cold start
+resumes1=($(seq 0 1 9))
+# parallel run
+resumes2=(9)
+resumes3=($(seq 9 1 197))
+resumes+=( "${resumes1[@]}" "${resumes2[@]}" "${resumes3[@]}")
+iterations=($(seq 1 1 200))
+for i in {0..199}; 
+do
+    python tdvi.py --content_path ${content_path} -i ${iterations[$i]} -r ${resumes[$i]} 1> ${content_path}/tdvi_log 2>&1
+done
+# ###############################################################
+content_path="/home/yangxl21/DVI_data/ResNet_FOOD101"
+
+resumes=($(seq 0 1 19))
+iterations=($(seq 1 1 20))
+for i in {0..19}; 
+do
+    python tdvi.py --content_path ${content_path} -i ${iterations[$i]} -r ${resumes[$i]} 1> ${content_path}/tdvi_log 2>&1
+done
+# ###############################################################
+content_path="/home/yangxl21/DVI_data/ViT_CIFAR10"
+
+resumes=($(seq 0 1 199))
+iterations=($(seq 1 1 200))
+
+for i in {0..199}; 
+do
+    echo ${iterations[$i]} ${resumes[$i]}
+    nohup python tdvi.py --content_path ${content_path} -i ${iterations[$i]} -r ${resumes[$i]} 1>> ${content_path}/tdvi_log 2>&1
+done
+# ###############################################################
+content_path="/home/yangxl21/DVI_data/ViT_CIFAR100"
+
+resumes=($(seq 0 1 199))
+iterations=($(seq 1 1 200))
+
+for i in {0..199}; 
+do
+    python tdvi.py --content_path ${content_path} -i ${iterations[$i]} -r ${resumes[$i]} 1> ${content_path}/tdvi_log 2>&1
+done
+# ###############################################################
+content_path="/home/yangxl21/DVI_data/ViT_FOOD101"
+
+resumes=($(seq 0 1 19))
+iterations=($(seq 1 1 20))
+
+for i in {0..19}; 
+do
+    python tdvi.py --content_path ${content_path} -i ${iterations[$i]} -r ${resumes[$i]} 1> ${content_path}/tdvi_log 2>&1
+done
+# ###############################################################
